e80eecaaccda791263a4a28fcbcba90cc3904aca
laurenspriem <laurens-pilot-git@lrns.me>
2026-07-16 13:11:35 +0530

    Update macOS app icon

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,6 +3,16 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    super.applicationDidFinishLaunching(notification)
+
+    if let iconURL = Bundle.main.url(forResource: "AppIcon", withExtension: "icns"),
+      let icon = NSImage(contentsOf: iconURL)
+    {
+      NSApplication.shared.applicationIconImage = icon
+    }
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
